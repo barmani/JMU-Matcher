1ef79149aff3c21c9e827bcb6f119360092c4ed4
Brendan Armani <brendanarmani@gmail.com>
2018-03-30 16:53:52 -0400

    Responses read into ArrayList

--- src/MatchCalculation.java
+++ src/MatchCalculation.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Methods to help calculate matches.
  * 
@@ -7,4 +13,22 @@
  */
 public class MatchCalculation {
 
+  /**
+   * Fill in the responses from the survey takers.
+   * 
+   * @param spreadsheet the csv unsplit spreadsheet
+   * @throws IOException ioe
+   */
+  public static ArrayList<String[]> fillAnswersArray(File spreadsheet) throws IOException {
+    ArrayList<String[]> allAnswers = new ArrayList<String[]>();
+    String line = "";
+    
+    BufferedReader reader = new BufferedReader(new FileReader(spreadsheet));
+    while ((line = reader.readLine()) != null) {
+      String[] arr = line.split("\",\"");
+      allAnswers.add(arr);
+    }
+    reader.close();
+    return allAnswers;
+  }
 }
--- src/MatchController.java
+++ src/MatchController.java
@@ -1,17 +1,21 @@
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MatchController {
 
   /**
    * Reads csv file, initiates match calculations, and writes to a text file.
    * @param args command line arguments
+   * @throws IOException ioe
    */
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     if (args.length != 1) {
       System.err.println("Must provide a file to read from.");
-    } else {
+    } else {  
       File spreadsheet = new File(args[0]);
-      System.out.println(spreadsheet.getPath());
+      // get answers in ArrayList from csv
+      ArrayList<String[]> allAnswers = MatchCalculation.fillAnswersArray(spreadsheet); 
     }
     
   }
